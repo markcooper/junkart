d9f3937e196c7822ae4bdeccb0753e516f8c7ba9
Mark Cooper <mark.v.cooper@gmail.com>
2011-11-28 21:26:04 -0500

    Initial Commit

---
+++ markFinal/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  markFinal
+//
+//  Created by eq2-mac on 11/28/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
